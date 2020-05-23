b4141914ecce7a627b7a07a01e5f61d4de3d9e97
Michael Lohier <lohiermichael@gmail.com>
2020-05-23 12:57:37 +0800

    Run the scripts for demo

--- section_4:machine_learning_pipeline:writing_production_code/3-third_party_pipeline/demo/pipeline.py
+++ section_4:machine_learning_pipeline:writing_production_code/3-third_party_pipeline/demo/pipeline.py
@@ -10,29 +10,29 @@ price_pipe = Pipeline(
     [
         ('categorical_imputer',
             pp.CategoricalImputer(variables=config.CATEGORICAL_VARS_WITH_NA)),
-         
-        ('numerical_inputer',
+
+        ('numerical_imputer',
             pp.NumericalImputer(variables=config.NUMERICAL_VARS_WITH_NA)),
-         
+
         ('temporal_variable',
             pp.TemporalVariableEstimator(
                 variables=config.TEMPORAL_VARS,
                 reference_variable=config.DROP_FEATURES)),
-         
+
         ('rare_label_encoder',
             pp.RareLabelCategoricalEncoder(
                 tol=0.01,
                 variables=config.CATEGORICAL_VARS)),
-         
+
         ('categorical_encoder',
             pp.CategoricalEncoder(variables=config.CATEGORICAL_VARS)),
-         
+
         ('log_transformer',
             pp.LogTransformer(variables=config.NUMERICALS_LOG_VARS)),
-         
+
         ('drop_features',
-            pp.DropUnecessaryFeatures(variables_to_drop=config.DROP_FEATURES)),
-         
+            pp.DropUnnecessaryFeatures(variables_to_drop=config.DROP_FEATURES)),
+
         ('scaler', MinMaxScaler()),
         ('Linear_model', Lasso(alpha=0.005, random_state=0))
     ]
