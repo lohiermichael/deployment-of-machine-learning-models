e6d55b7bc1be2210bbdefa4c7787fb8f64221e1f
Michael Lohier <lohiermichael@gmail.com>
2020-05-26 23:23:58 +0800

    Complete the config file

--- section_4:machine_learning_pipeline:writing_production_code/3-third_party_pipeline/assignment/personal/config.py
+++ section_4:machine_learning_pipeline:writing_production_code/3-third_party_pipeline/assignment/personal/config.py
@@ -6,10 +6,10 @@ PIPELINE_NAME = 'logistic_regression.pkl'
 
 # ======= FEATURE GROUPS =============
 
-TARGET = 
+TARGET = 'survived'
 
-CATEGORICAL_VARS = 
+CATEGORICAL_VARS = ['sex', 'cabin', 'embarked', 'title']
 
-NUMERICAL_VARS = 
+NUMERICAL_VARS = ['pclass', 'age', 'sibsp', 'parch', 'fare']
 
-CABIN = 
+CABIN = 'cabin'
