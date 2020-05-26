fa888a60d0d10b1b9f46c7d7e20f220655cdc4cb
Michael Lohier <lohiermichael@gmail.com>
2020-05-26 23:40:38 +0800

    Write pipeline

--- section_4:machine_learning_pipeline:writing_production_code/3-third_party_pipeline/assignment/personal/pipeline.py
+++ section_4:machine_learning_pipeline:writing_production_code/3-third_party_pipeline/assignment/personal/pipeline.py
@@ -7,6 +7,17 @@ import config
 
 
 titanic_pipe = Pipeline(
-    # complete with the list of steps from the preprocessors file
-    # and the list of variables from the config
-    )
+    [
+        ('extract_first_letter', pp.ExtractFirstLetter(variables=config.CABIN)),
+        ('missing_indicator', pp.MissingIndicator(variables=config.NUMERICAL_VARS)),
+        ('numerical_imputer', pp.NumericalImputer(variables=config.NUMERICAL_VARS)),
+        ('categorical_imputer', pp.CategoricalImputer(
+            variables=config.CATEGORICAL_VARS)),
+        ('rare_label_encoder',
+            pp.RareLabelCategoricalEncoder(
+                tol=0.01,
+                variables=config.CATEGORICAL_VARS)),
+        ('categorical_encoder', pp.CategoricalEncoder(
+            variables=config.CATEGORICAL_VARS))
+    ]
+)
