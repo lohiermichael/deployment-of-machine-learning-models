06f2181310fd775ee34e4499bb0bb3892be63ae0
Michael Lohier <lohiermichael@gmail.com>
2020-05-26 23:45:28 +0800

    Complete train_pipeline file

--- section_4:machine_learning_pipeline:writing_production_code/3-third_party_pipeline/assignment/personal/train_pipeline.py
+++ section_4:machine_learning_pipeline:writing_production_code/3-third_party_pipeline/assignment/personal/train_pipeline.py
@@ -8,18 +8,25 @@ from pipeline import titanic_pipe
 import config
 
 
-
 def run_training():
     """Train the model."""
 
     # read training data
+    data = pd.read_csv(config.TRAINING_DATA_FILE)
 
     # divide train and test
+    X_train, X_test, y_train, y_test = train_test_split(
+        data[config.FEATURES],
+        data[config.TARGET],
+        test_size=0.2,
+        random_state=0)
 
     # fit pipeline
+    titanic_pipe.fit(X_train, y_train)
 
     # save pipeline
-    
+    joblib.dump(titanic_pipe, config.PIPELINE_NAME)
+
 
 if __name__ == '__main__':
     run_training()
